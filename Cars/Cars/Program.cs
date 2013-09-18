af45a2c0569543b25f5511226d26c53f645ddcea
Michele Stoltz <micsto92@gmail.com>
2013-09-18 09:53:37 +0200

    I added a few sweet rides!

--- Cars/Cars/Program.cs
+++ Cars/Cars/Program.cs
@@ -7,6 +7,8 @@ class Program
         public static void Main()
         {
             string carOne = "Green";
+            string carTwo = " I am bluje";
+            int carThree = "I am blue dadbadebadidajjj";
             Console.WriteLine("Hi, I'm a caaar and I am " + carOne);
         }
 }

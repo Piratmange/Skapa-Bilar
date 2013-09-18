5fdfe83a90a57bba79bfa7b15d2befad6ab26a0e
AxelPalmqvist <axel.palmqvist@gmail.com>
2013-09-18 08:46:59 +0100

    Added one car that is green.

---
+++ Cars/Cars/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+class Program
+{
+
+        public static void Main()
+        {
+            string carOne = "Green";
+            Console.WriteLine("Hi, I'm a caaar and I am " + carOne);
+        }
+}
+

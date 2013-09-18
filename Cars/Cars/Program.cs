3cc2b8bc9b5f28c3c9b43cc6d8a5e0984f959d81
AxelPalmqvist <axel.palmqvist@gmail.com>
2013-09-18 09:54:43 +0100

    bitches bro, bitches

--- Cars/Cars/Program.cs
+++ Cars/Cars/Program.cs
@@ -6,9 +6,13 @@ class Program
 
     public static void Main()
     {
+        
+
         string bitch = " ";
+        int bitchInt;
         Console.WriteLine("What is your girlfriends age?");
         bitch = Console.ReadLine();
+        bitchInt = Convert.ToInt32(bitch);
         Console.Clear();
 
         string money = " ";
@@ -21,7 +25,9 @@ class Program
         car = Console.ReadLine();
         Console.Clear();
 
-        Console.WriteLine("Your girlfriend age is " + bitch + " and a " + car + "and you make " + money + " a month");
+        Console.WriteLine(bitchInt > 25 ? "Lose" : "win"); 
+
+        Console.WriteLine("Your girlfriends age is " + bitch + " and you drive a " + car + "and you make " + money + " a month");
     }
 }
 

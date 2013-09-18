20fe0d3bf9780ba8cff9d4dec8fd14764f844679
Michele Stoltz <micsto92@gmail.com>
2013-09-18 10:09:51 +0200

    Added a sweet girl

--- Cars/Cars/Program.cs
+++ Cars/Cars/Program.cs
@@ -6,14 +6,17 @@ class Program
 
     public static void Main()
     {
-        Console.WriteLine("How many cars do you own and what color are they?");
+        Console.WriteLine("How many cars do you own, what color are they and whats your bitch called?");
+     
         Console.ReadLine();
 
+        string hotChick = "Gunilla"
         string carOne = "Green";
         string carTwo = " Blue";
         string carThree = "Red";
 
-        Console.WriteLine("Hi, I'm a caaar and I am " + carOne + "My brother is " + carTwo + "and my sister is" + carThree);
+        Console.WriteLine("Hi, I'm a caaar and I am " + carOne + "My brother is " + carTwo + "and my sister is" 
+            + carThree + "and i got a hot bitch at home called" + hotChick);
     }
 }
 
